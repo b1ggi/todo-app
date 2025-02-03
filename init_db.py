f544fb4348fe565366ab23739d56470af170448c
b1ggi <5243849+b1ggi@users.noreply.github.com>
2025-02-04 00:00:35 +0100

    init_db fixes prepend app.

--- init_db.py
+++ init_db.py
@@ -3,18 +3,18 @@ with app.app_context():
     db.create_all()
 
     #Check if the default project exists
-    default_project = Project.query.filter_by(is_default=True).first()
+    default_project = app.Project.query.filter_by(is_default=True).first()
     if not default_project:
         #Create the default project
-        default_project = Project(name="Default Project", is_default=True)
+        default_project = app.Project(name="Default Project", is_default=True)
         db.session.add(default_project)
         db.session.commit()
     
     
     #Check if the default list exists
-    default_list = List.query.filter_by(is_default=True).first()
+    default_list = app.List.query.filter_by(is_default=True).first()
     if not default_list:
         #Create the default list
-        default_list = List(name="Default List", project_id=default_project.id, is_default=True)
+        default_list = app.List(name="Default List", project_id=default_project.id, is_default=True)
         db.session.add(default_list)
         db.session.commit()
